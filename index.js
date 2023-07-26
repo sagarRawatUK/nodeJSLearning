62469bb1857c18d589a66298a64c3f8f5925ebdd
sagarRawat-cardbyte <sagar.r@antino.io>
2023-07-26 12:38:37 +0530

    Added express and env

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-const http = require("http");
+require("dotenv").config();
+const express = require("express");
+var app = express();
 
-const jsonResponse = {
-    firstName: "Sagar",
-    lastName: "Rawat"
-}
-const server = http.createServer((request, response) => {
-
-    if (request.url === '/') {
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.end("Home Page");
-    } else if (request.url === '/api') {
-        response.writeHead(200, { "Content-Type": "application/json" });
-        response.end(JSON.stringify(jsonResponse));
-    } else if (request.url === '/about') {
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.end("About Page");
-    } else if (request.url === '/help') {
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.end("Help Page");
-    } 
-});
-
-server.listen(3000, () => {
-    console.log("Server running on port 3000")
+app.get("/", function (request, response) {
+    response.send("Hello World!");
+})
+app.listen(process.env.PORT, function () {
+    console.log(`Started application on port ${process.env.PORT}`);
 });
