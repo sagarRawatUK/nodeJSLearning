275f155845e5a4a2c84729773dce2167f538840f
sagarRawat-cardbyte <sagarrawatuk@gmail.com>
2023-08-02 14:20:12 +0530

    Nodemailer mail added

--- src/auth/controller/auth.controller.js
+++ src/auth/controller/auth.controller.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
 const User = require('../model/auth.model.js')
+const nodemailer = require('nodemailer')
 
 
 const register = async (req, res) => {
@@ -46,22 +47,52 @@ const login = async (req, res) => {
     }
 };
 
-const forgotPassword = async (req, res) => {
+const forgotPassword = async (req, res, next) => {
+    const { email } = req.body;
     try {
-        const { email } = req.body;
         const user = await User.findOne({ email });
         if (!user) {
-            return res.status(401).json({ message: "This email is not registered with us" });
+            return res.status(404).json({ message: 'User not found' });
         }
-        res.status(200).json({ message: "Password reset link sent successfully"});
-        
+        console.log(process.env.USER)
+        console.log(process.env.PASS)
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail', 
+            auth: {
+                user: 'sagarrawatuk3@gmail.com',
+                pass: process.env.PASS,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.USER,
+            to: user.email,
+            subject: 'Reset Password Request',
+            text: 'Click the link below to reset your password:',
+            html: `<p>Click the link below to reset your password:</p><a href="http://your-frontend-app/reset-password/">Reset Password</a>`,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email:', error);
+                return res.status(500).json({ message: 'Error sending email' });
+            }
+
+            console.log('Email sent:', info.response);
+            return res.status(200).json({ message: 'Email sent successfully' });
+        });
+
+
+
+
+    } catch (error) {
+
+        console.error('Forgot password error:', error);
+        return res.status(500).json({ message: 'Something went wrong' });
+
     }
-    catch (error) {
-        console.log(`/login error ${error}`);
-        console.log(error)
-        res.status(500).json({ message: "Failed to login" });
-    }
-};
+}
 
 
 module.exports = {
